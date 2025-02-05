30be7edab60d586d0a06a51562cc211bb0be4cc3
BrianMohseni <157198911+BrianMohseni@users.noreply.github.com>
2025-02-05 14:39:33 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,7 +4,6 @@ model_id = "Qwen/Qwen2.5-3B-Instruct"
 pipe = transformers.pipeline(
     "text-generation",
     model=model_id,
-    load_in_4bit=True,
     device_map="auto",
 )
 
@@ -13,16 +12,19 @@ messages = [
 ]
 
 while True:
-    prompt = input("prompt: ")
+    prompt = input("Prompt: ")
 
     if prompt == "exit":
         break
+
+    prompt = {"role": "user", "content"c: prompt}
+    
     messages.append(prompt)
     outputs = pipe(
         messages,
         max_new_tokens=256,
     )
-
-    print(outputs)
-    
+    outputs = outputs[0]["generated_text"][-1]["content"]
+    print("Response: ", outputs)
+    outputs = {"role": "assistant", "content": outputs}
     messages.append(outputs)
